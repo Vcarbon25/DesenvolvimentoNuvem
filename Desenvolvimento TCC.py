b1828d0d25150c7c318dc4d0a186b15a8529a9bf
Victor Carbón <victorcarbon@gmail.com>
2022-03-30 09:52:34 -0300

    Capaz de fechar a janela da camera

--- Desenvolvimento TCC.py
+++ Desenvolvimento TCC.py
@@ -6,4 +6,7 @@ while True: #esse loop nao pode ser interrompido
     sucess, frame = cap.read() #desempacotamento dos dados
 
     cv2.imshow("camera", frame) #abre um popout com a imagem enviada,e o titulo escolhido
-    cv2.waitKey(1)
+    if cv2.waitKey(10)&0xFF == ord('q'):  #observa se determinada tecla foi pressionada para executar,no caso q
+        break
+cap.close()     #fecha a imagem da camera
+cv2.destroyAllWindows()     #fecha a janela de display
