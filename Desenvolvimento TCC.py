dbeab909c366f9f570e08558d6bafc83ff2033cb
Vcarbon25 <carbonvictor@gmail.com>
2022-04-14 13:36:56 -0300

    Com contagem de fps

--- Desenvolvimento TCC.py
+++ Desenvolvimento TCC.py
@@ -1,12 +1,15 @@
 import cv2
 from cv2 import COLOR_RGB2BGR #biblioteca necessária para o uso da camera
 import mediapipe as mp #bibliotecapara a detecção da postura
+import time
 
 #prepara as ferramentas para chamada facil
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
 cap = cv2.VideoCapture(0) #aqui o argumento da função será o indice da câmera utilizada
+ptime=0
+ctime=0
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic: #determina as especificações e o simbolo do modelo holistico utilizado
     while cap.isOpened(): #loop enquanto camera aberta
         sucess, frame = cap.read() #desempacotamento dos dados
@@ -19,7 +22,14 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         mp_drawing.draw_landmarks(imagemBGR, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
         mp_drawing.DrawingSpec(color=(100,255,100),thickness=4,circle_radius=6),
         mp_drawing.DrawingSpec(color=(100,100,255),thickness=3,circle_radius=5))
+        ctime=time.time()
+        fps=1/(ctime-ptime)
+        ptime=ctime
+        nfps=int(fps)
+        txtfps=str(nfps)
+        cv2.putText(imagemBGR,txtfps,(10,50),cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,50),3)
         cv2.imshow("Detecção de POSE", imagemBGR) #abre um popout com a imagem enviada,e o titulo escolhido
+        
         if cv2.waitKey(10)&0xFF == ord('n'):
             x = results.pose_world_landmarks.landmark[0].x
             y = results.pose_world_landmarks.landmark[0].y
