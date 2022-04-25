7c249d14c6e0d641478104de7c7a9ab8a5448220
Vcarbon25 <carbonvictor@gmail.com>
2022-04-25 11:01:10 -0300

    Fecha a câmera com ESC

--- Camera.py
+++ Camera.py
@@ -1,8 +1,11 @@
 import cv2
 
 cap = cv2.VideoCapture(0) #argumento é o indice da camera utilizada
-while True: #Fica em Loop Infinito sem interromper a menos que feche IDE
+while cap.isOpened(): #Fica em Loop enquanto a janela aberta
     sucess, img = cap.read()
 
     cv2.imshow('Camera',img)
-    cv2.waitKey(1)
+    if cv2.waitKey(10)&0xFF == 27:  #fecha Com ESC
+            break
+cap.release()     #fecha a imagem da camera
+cv2.destroyAllWindows()     #fecha a janela de display
