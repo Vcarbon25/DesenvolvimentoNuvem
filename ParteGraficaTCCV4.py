8c2157a567b5e6b6cb5c2b8a51746945db2fef52
Vcarbon25 <carbonvictor@gmail.com>
2022-05-06 17:23:41 -0300

    A pessoa ficou azul mais por enquanto é isso

--- ParteGraficaTCCV4.py
+++ ParteGraficaTCCV4.py
@@ -1,8 +1,7 @@
 import tkinter as TK
 import cv2
 from PIL import Image, ImageTk
-
-from ParteGraficaTCCV2 import ListaCameras
+import mediapipe as mp
 
 raiz = TK.Tk()
 
@@ -22,7 +21,7 @@ def show_frames(cv2image):
    LCamera.imgtk = imgtk
    LCamera.configure(image=imgtk)
    # Repeat after an interval to capture continiously
-   LCamera.after(20, CameraSimples)
+   LCamera.after(20, Modelo_Corpo)
 
 def Encontrar_Cameras():
     ListaCameras=[]
@@ -40,8 +39,18 @@ LCamera =TK.Label(raiz)
 LCamera.grid(row=0, column=0,rowspan=6)
 botao = TK.Button(raiz,text='but1',padx=30)
 botao.grid(row=0,column=1)
-
-
+def Modelo_Corpo():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_holistic = mp.solutions.holistic
+    with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5) as holistic:
+        sucess, frame=cap.read()
+        imagemRGB=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        results = holistic.process(imagemRGB)
+        imagemBGR = cv2.cvtColor(imagemRGB,cv2.COLOR_RGB2BGR)
+        mp_drawing.draw_landmarks(imagemBGR,results.pose_landmarks,mp_holistic.POSE_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(100,255,100),thickness=4,circle_radius=6),
+        mp_drawing.DrawingSpec(color=(100,100,255),thickness=3,circle_radius=5))
+        show_frames(imagemBGR)
 
 CameraSimples()
 raiz.mainloop()
