73a43f44557d388a31121d7033fcb3f81900b866
Vcarbon25 <carbonvictor@gmail.com>
2022-05-10 10:53:34 -0300

    Criou objeto da classe no ar auxiliar

--- Tela PySimpleGui/ParteGraficaTCCV5.py
+++ Tela PySimpleGui/ParteGraficaTCCV5.py
@@ -1,12 +1,12 @@
 from PySimpleGUI import PySimpleGUI as sg
 import cv2
-#import AuxiliarV5
+import AuxiliarV5
 sg.theme('SandyBeach')
 
 camera_Width=320
 camera_heigth = 240
 Tamanho_Imagem=(camera_Width,camera_heigth)
-#Sistema_De_Cameras = AuxiliarV5.Cameras_TCC
+Sistema_De_Cameras = AuxiliarV5.CamerasTCC
 cameras = [0,1] #Sistema_De_Cameras.Listar_cameras()
 Exames=['Camera Simples','Angulo Corporal']
 coluna_esquerda = [[sg.Image(filename="",key="camera")],[sg.Output(20,4)]]
