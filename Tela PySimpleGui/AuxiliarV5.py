fe644629a45a9cf578a8ebce872cbcc4491f6199
Vcarbon25 <carbonvictor@gmail.com>
2022-05-10 10:41:47 -0300

    V5 abre a janela mas sem consultar arquivo auxiliar

---
+++ Tela PySimpleGui/AuxiliarV5.py
@@ -0,0 +1,20 @@
+import cv2
+
+class Cameras_TCC:
+    global HabilitarCameras
+    HabilitarCameras=False
+    def __init__(self):
+       pass
+    def Listar_cameras():
+        index = 0
+        arr = []
+        while HabilitarCameras:
+            cap = cv2.VideoCapture(index)
+            if not cap.read()[index]:
+                break
+            else:
+              arr.append(index)
+            cap.release()
+            index += 1
+        return arr
+    
---
+++ Tela PySimpleGui/ParteGraficaTCCV5.py
@@ -0,0 +1,23 @@
+from PySimpleGUI import PySimpleGUI as sg
+import cv2
+#import AuxiliarV5
+sg.theme('SandyBeach')
+
+camera_Width=320
+camera_heigth = 240
+Tamanho_Imagem=(camera_Width,camera_heigth)
+#Sistema_De_Cameras = AuxiliarV5.Cameras_TCC
+cameras = [0,1] #Sistema_De_Cameras.Listar_cameras()
+Exames=['Camera Simples','Angulo Corporal']
+coluna_esquerda = [[sg.Image(filename="",key="camera")],[sg.Output(20,4)]]
+coluna_do_meio = [[],[sg.Text('Camera: '),sg.OptionMenu(cameras,default_value='camera')],
+[sg.Text('Selecione O Exame :'), sg.OptionMenu(Exames,default_value='escolha')],
+[sg.Button('Iniciar Exame'),sg.Button('Interromper')]]
+
+layout =[coluna_esquerda,coluna_do_meio]
+principal = sg.Window('V3.5 do TCC',layout).finalize()
+
+while True:
+    eventos, valores = principal.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
