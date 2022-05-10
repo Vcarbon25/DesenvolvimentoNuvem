e9ab96660035ae91a4ce0fef967b0d569ac39d81
Vcarbon25 <carbonvictor@gmail.com>
2022-05-10 12:53:02 -0300

    Quando removi o arquivo auxiliar ele rodou sem problemas

--- Tela PySimpleGui/ParteGraficaTCCV5.py
+++ Tela PySimpleGui/ParteGraficaTCCV5.py
@@ -1,13 +1,26 @@
 from PySimpleGUI import PySimpleGUI as sg
 import cv2
-import AuxiliarV5
+
 sg.theme('SandyBeach')
+
+def Listar_cameras():
+    index=0
+    lista = []
+    while True:
+        cap = cv2.VideoCapture(index)
+        if not cap.read()[0]:
+            break
+        else:
+            lista.append(index)
+        cap.release()
+        index+=1
+    return lista
+       
 
 camera_Width=320
 camera_heigth = 240
 Tamanho_Imagem=(camera_Width,camera_heigth)
-Sistema_De_Cameras = AuxiliarV5.CamerasTCC
-cameras = [0,1] #Sistema_De_Cameras.Listar_cameras()
+cameras = Listar_cameras()#AuxiliarV5.CamerasTCC.Listar_cameras()
 Exames=['Camera Simples','Angulo Corporal']
 coluna_esquerda = [[sg.Image(filename="",key="camera")],[sg.Output(20,4)]]
 coluna_do_meio = [[],[sg.Text('Camera: '),sg.OptionMenu(cameras,default_value='camera')],
